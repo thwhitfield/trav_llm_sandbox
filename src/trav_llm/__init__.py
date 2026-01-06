765397b7e5baf1eff44a8e6bf775c293dc195d34
Travis Whitfield <thwhitfield@gmail.com>
2026-01-05 21:10:38 -0500

    Add pyproject.toml, update the environment, and add the scaffolding for the package

---
+++ src/trav_llm/__init__.py
@@ -0,0 +1,8 @@
+"""trav_llm - Sandbox to programmatically test out models, prompts, etc."""
+
+__version__ = "0.1.0"
+
+# Import main classes/functions here to make them available at package level
+# Example:
+# from .llm import YourMainClass
+# __all__ = ["YourMainClass"]
